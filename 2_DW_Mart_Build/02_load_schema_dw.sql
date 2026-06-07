c0caff5e891cbcc735bea447b91cc39615d15922
conrad85E <marvin777@bk.ru>
2026-06-07 13:43:01 +0500

    data validation check sat

--- 2_DW_Mart_Build/02_load_schema_dw.sql
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -46,3 +46,25 @@ SELECT
     job_id
 FROM read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv')
 ;
+
+SELECT 'Company Dim' AS table_name, COUNT(*) AS record_count FROM company_dim
+UNION
+SELECT 'Skills Dim', COUNT(*) FROM skills_dim
+UNION
+SELECT 'Job Postings Fact', COUNT(*) FROM job_postings_fact
+UNION
+SELECT 'Skills Job Dim', COUNT(*) FROM skills_job_dim;
+
+
+
+SELECT '=== Company Dimension Sample ===' AS info;
+SELECT * FROM company_dim LIMIT 5;
+
+SELECT '=== Skills Dimension Sample ===' AS info;
+SELECT * FROM skills_dim LIMIT 5;
+
+SELECT '=== Job Postings Fact Sample ===' AS info;
+SELECT * FROM job_postings_fact LIMIT 5;
+
+SELECT '=== Skills Job Bridge Sample ===' AS info;
+SELECT * FROM skills_job_dim LIMIT 5;
